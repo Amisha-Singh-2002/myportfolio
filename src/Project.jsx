9ee711093bcd64d59e126accc2a0fd9f653cab08
Amisha-Singh-2002 <amishasingh2302@gmail.com>
2024-06-20 01:39:31 +0530

    message

--- src/Project.jsx
+++ src/Project.jsx
@@ -4,6 +4,7 @@ import Cal from "./assets/Cal.png"
 import DP from "./assets/DP.jpg"
 import Card from "./Card.jsx"
 import AOS from 'aos';
+import GOLOCAL from "./assets/golocal.jpg"
 import 'aos/dist/aos.css'; 
 const Project=()=> {
   AOS.init();
@@ -17,7 +18,7 @@ const Project=()=> {
                     <Card img={Cal} name="Calculator" desc="Introducing our sleek, HTML, CSS, and JavaScript-based calculator for seamless, precise calculations with a user-friendly interface and customizable themes. " view="https://main--superlative-sherbet-ba0ad4.netlify.app/" git="https://github.com/Amisha-Singh-2002/calculator.git" />
             </div>
             <div className='projectcard' data-aos="fade-up-left">
-                    <Card img={Cal} name="Calculator" desc="Introducing our sleek, HTML, CSS, and JavaScript-based calculator for seamless, precise calculations with a user-friendly interface and customizable themes. " view="https://www.google.com" git="githud.com/7twik" />
+                    <Card img={GOLOCAL} name="E-vendor" desc="My best Project so far, deFi+live location tracking+ecommerce app for both sellers and customer with admin side, decentralized rating " view="https://golocal-ktqh.onrender.com/" git="https://github.com/Amisha-Singh-2002/GoLocal" />
             </div>
         </div>
     </div>
